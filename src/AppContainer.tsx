facb709b2d6c648f07141fc51b005e0b2fa7361a
FD000 <fethullah.dincer@gmail.com>
2025-12-13 00:32:09 +0100

    material ui för logga in

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './pages/loginPage';
-
+import LandingPage from './pages/LandingPage';
+/* LandingPage */
 const AppContainer = () => {
   return (
-    <LoginPage />
+    <LandingPage />
   );
 }
 
