6c4023920514f5dd533a52f6c99d2c63ddd54821
FD000 <fethullah.dincer@gmail.com>
2025-12-13 00:09:58 +0100

    first Push

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AppContainer from './AppContainer';
+
+function App() {
+  return (
+    <AppContainer />
+  );
+}
+
+export default App;
---
+++ src/AppContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './pages/loginPage';
+
+const AppContainer = () => {
+  return (
+    <LoginPage />
+  );
+}
+
+export default AppContainer;
